e99c2b65a921826a95575e3c879476e194548f36
noobexplore <449053131@qq.com>
2020-07-15 15:22:06 +0800

    07/15.am fixed this file for better vision

--- train.py
+++ train.py
@@ -6,9 +6,11 @@
 # @File    : train.py
 # @Software: PyCharm
 import time
+import numpy as np
 import tensorflow as tf
-from data_utils import BatchManager, get_dict
+from data_utils import BatchManager, get_dict, make_path, get_logger
 from model import Model
+from params_utils import get_params
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -16,33 +18,51 @@ warnings.filterwarnings("ignore")
 old_v = tf.logging.get_verbosity()
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-batch_size = 32
-dict_file = './datas/prepare_data/dict.pkl'
-
 
 def train(param):
+    # 检查参数
+    assert param.clip < 5.1, "gradient clip should't be too much"
+    assert 0 <= param.dropout < 1, "dropout rate between 0 and 1"
+    assert param.lr > 0, "learning rate must larger than zero"
     # 数据准备
-    train_manager = BatchManager(batch_size, name='train')
+    train_manager = BatchManager(param.batch_size, name='train')
+    number_dataset = train_manager.len_data
+    print("total of number train data is {}".format(number_dataset))
+    # 创建相应的文件夹
+    make_path(param)
+    # 配置日志
+    logger = get_logger(param.train_log_file)
     # 读取字典
-    mapping_dict = get_dict(dict_file)
+    mapping_dict = get_dict(param.dict_file)
     # 搭建模型
-    model = Model(mapping_dict)
+    model = Model(param, mapping_dict)
     # 初始化参数
     init = tf.global_variables_initializer()
-    with tf.Session() as sess:
+    # 配置GPU参数
+    gpu_config = tf.ConfigProto(log_device_placement=True, allow_soft_placement=True)
+    with tf.Session(config=gpu_config) as sess:
         sess.run(init)
-        for i in range(10):
-            j = 1
+        for i in range(param.max_epoch):
+            loss = []
+            total_loss = 0
+            # 初始化时间
+            start = time.time()
             for batch in train_manager.iter_batch(shuffle=True):
-                start = time.time()
-                loss = model.run_step(sess, batch)
-                end = time.time()
-                if j % 10 == 0:
-                    print("epoch:{}, step:{}/{}, loss:{}, elapse:{}, estimate:{}".
-                          format(i, j, train_manager.len_data, loss, end - start,
-                                 (end - start) * (train_manager.len_data - j)))
-                j += 1
+                step, batch_loss = model.run_step(sess, batch)
+                # 这里计算平均loss
+                loss.append(batch_loss)
+                # 这里计算总的loss后面计算全部平均
+                total_loss += batch_loss
+                if step % 5 == 0:
+                    logger.info("epoch:{}, step:{}/{}, avg_loss:{:>9.6f}".format(i + 1, step, train_manager.len_data,
+                                                                                 np.mean(loss)))
+            # 每两个周期保存一次
+            if (i + 1) % 2 == 0:
+                model.save_model(sess, logger)
+            logger.info('Epoch {}, total Loss {:.4f}'.format(i + 1, total_loss / train_manager.len_data))
+            logger.info('Time taken for one epoch {:.4f} sec\n'.format(time.time() - start))
 
 
 if __name__ == '__main__':
-    train()
+    params = get_params()
+    train(params)
